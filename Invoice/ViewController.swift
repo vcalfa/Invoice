2fb7778935cda4b9dc6255c62bdeaf7f75bc102d
Vladimir Calfa <vcalfa@gmail.com>
2022-06-18 20:40:57 +0200

    Initial Commit

---
+++ Invoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Invoice
+//
+//  Created by Vladimir Calfa on 18/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
