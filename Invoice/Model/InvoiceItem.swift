03bfa203eed362d641e305d27863c5ab48632972
Vladimir Calfa <vcalfa@gmail.com>
2022-07-04 18:31:55 +0200

    Removed unnecessary extension

--- Invoice/Model/InvoiceItem.swift
+++ Invoice/Model/InvoiceItem.swift
@@ -68,6 +68,6 @@ extension InvoiceItem {
         let image = ["IMG_1529", "IMG_1530", "IMG_1531"].randomElement().flatMap { UIImage(named: $0) }
         let currency = ["CZK", "USD", "EUR", "DKK"].randomElement() ?? "EUR"
         let date = Date.randomBetween(start: Date.parse("2020-01-01"), end: Date())
-        return InvoiceItem(invoiceId: nil, date: date, total: Double.random(min: 0.00, max: 9900.0), currencyCode: currency, note: Lorem.sentences(1), image: image, imageId: nil)
+        return InvoiceItem(invoiceId: nil, date: date, total: Double.random(in: 0.00 ... 9900.0), currencyCode: currency, note: Lorem.sentences(1), image: image, imageId: nil)
     }
 }
